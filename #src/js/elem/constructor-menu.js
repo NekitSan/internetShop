43be8d847e3c920415c04c9ba9f8a6226a459dbb
NekitSan <zonaneka71@mail.ru>
2020-07-16 23:39:51 +0300

    edit page constructor2

--- #src/js/elem/constructor-menu.js
+++ #src/js/elem/constructor-menu.js
@@ -1,19 +1,38 @@
 $(document).ready(function()
 {
-    let color = new Array($("#color_item"),$("#color_block"));
+    maxSizeBlock($(".text_description"),322,$(".active_menu-description"));
 
-
-    activeMenu(color[0],color[1]);
+    /* Active Menu */
+    maxSizeBlock($("#constructor_color"),42,$("#color_block .list-constructor"));
+    maxSizeBlock($("#constructor_size"),42,$("#size_block .list-constructor"));
+    maxSizeBlock($("#configuration_block"),42,$("#configuration_block .list-constructor"));
+    maxSizeBlock($("#constructor_equipment"),42,$("#equipment_block .list-constructor"));
+    maxSizeBlock($("#backlight"),42,$("#backlight_block .list-constructor"));
+    /* #Active Menu# */
 });
 
+/* 
+ **Подставляет скролл, если высота привышает родителя 
+ * name - название блока, у которого берется высота
+ * h - максимальная высота
+ * block - блок которому будет применяться скролл
+*/
+function maxSizeBlock(name, h, block)
+{
+    var h_block = name.height();
+    if(h_block > h)
+    {
+        block.css("overflow-y","scroll");
+    }
+}
 
-function activeMenu (e,f)
+function activeMenu (e,f,key)
 {
     e.click(function () {
         var id = f.attr('id');
-        if(id == "color_block")
-            f.attr('id',"click");
+        if(id == key)
+            f.attr('id', "click");
         else
-            f.attr('id',"color_block");
+            f.attr('id', key);
 	});
 }
--- #src/js/elem/menu.js
+++ #src/js/elem/menu.js
@@ -1,18 +1,19 @@
 $(document).ready(function() {
+    /* Для телефонов */
     let numOfClicks = 0;
-    const $elem = $('.menu-title');
+    const elem = $('.menu-title');
 
-    $elem.on('click', () => {
+    elem.on('click', () => {
         ++numOfClicks;
         if(numOfClicks % 2 !== 0)
         { 
             $('.inner_menu').css("display","block");
-            $elem.css("border-bottom","solid 3px #fff");
+            elem.css("border-bottom","solid 3px #fff");
         }
         else 
         {
             $('.inner_menu').css("display","");
-            $elem.css("border-bottom","");
+            elem.css("border-bottom","");
         }
     });
 });
